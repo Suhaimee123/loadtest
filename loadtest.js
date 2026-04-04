98b6684687de2def4e1b061a8a800f9f8dbd9a62
Suhaimee <suhaimee.kj@gmail.com>
2026-04-05 03:55:29 +0700

    chore: automate load tests via cron and adjust k6 test duration and VU capacity

--- loadtest.js
+++ loadtest.js
@@ -6,9 +6,9 @@ const status503Counter = new Counter('http_req_status_503');
 
 export const options = {
   stages: [
-    { duration: '30s', target: 100 },
-    { duration: '10m', target: 100 },
-    { duration: '30s', target: 0 },
+    { duration: '1m', target: 20 },
+    { duration: '20m', target: 20 },
+    { duration: '1m', target: 0 },
   ],
   thresholds: {
     'http_req_duration{name:1. Verify Override Token}': [],
@@ -220,7 +220,7 @@ export function handleSummary(data) {
 
   return {
     'summary.json': JSON.stringify(summaryData),
-    'stdout': `\n🚀 Detailed Load Test Summary (VUs: 100, 503s: ${s503Count}, Time: 10m)\n` +
+    'stdout': `\n🚀 Detailed Load Test Summary (VUs: 20, 503s: ${s503Count}, Time: 20m)\n` +
               `--------------------------------------------------------------------------------\n` +
               `Endpoint                   | Avg(ms) | P95(ms) | Min(ms) | Max(ms) | Fails | Error%\n` +
               `--------------------------------------------------------------------------------\n` +
